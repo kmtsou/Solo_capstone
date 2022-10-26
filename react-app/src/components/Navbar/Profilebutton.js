eec20e6b44a4647f7e804c803ae4877c745c5b22
kevin <kmtsou@sbcglobal.net>
2022-10-26 16:49:10 -0700

    added styling to login/signup, profilebutton skeleton

--- react-app/src/components/Navbar/Profilebutton.js
+++ react-app/src/components/Navbar/Profilebutton.js
@@ -1,14 +1,50 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { NavLink, useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../store/session';
 
-function ProfileButton({sessionUser}) {
+function ProfileButton({ sessionUser }) {
     const dispatch = useDispatch();
+    const history = useHistory();
     const [showMenu, setShowMenu] = useState(false);
 
+    const openMenu = () => {
+        if (showMenu) return;
+        setShowMenu(true);
+    };
+
+    useEffect(() => {
+        if (!showMenu) return;
+        const closeMenu = () => {
+            setShowMenu(false);
+        };
+        document.addEventListener('click', closeMenu);
+        return () => document.removeEventListener('click', closeMenu);
+    }, [showMenu])
+
+    const submitLogOut = async (e) => {
+        e.preventDefault();
+        let loggedOut = await dispatch(logout());
+        return history.push('/')
+    };
+
     return (
-        <div></div>
+        <div className='navright-profile-div'>
+            <div className='profile-button-container'>
+                <button onClick={openMenu} className='profile-dropdown'>
+                    <div className='profile-icon-div'>
+                        <i className="fas fa-user-circle fa-2x" />
+                    </div>
+                </button>
+            </div>
+            {showMenu && sessionUser && (
+                <ul className='profile-details'>
+                    <li className='profile-details-li'>
+                        <div className='signout-div' onClick={submitLogOut}>Log out</div>
+                    </li>
+                </ul>
+            )}
+        </div>
     )
 }
 
