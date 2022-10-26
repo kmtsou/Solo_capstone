17492fcba3d8e4f0596f1880ee55b45824b02124
kevin <kmtsou@sbcglobal.net>
2022-10-25 17:01:38 -0700

    added community model

--- app/models/user.py
+++ app/models/user.py
@@ -11,6 +11,8 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
+    community = db.relationship("Community", back_populates="owner", cascade="all, delete")
+
     @property
     def password(self):
         return self.hashed_password
