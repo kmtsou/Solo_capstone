259526f58340b8da6825810b05f65b5e70dd595b
kevin <kmtsou@sbcglobal.net>
2022-11-04 10:16:56 -0700

    created comment routes file

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.community_routes import community_routes
 from .api.post_routes import post_routes
+from .api.comment_routes import comment_routes
 
 from .seeds import seed_commands
 
@@ -35,6 +36,7 @@ app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(community_routes)
 app.register_blueprint(post_routes)
+app.register_blueprint(comment_routes)
 db.init_app(app)
 Migrate(app, db)
 
